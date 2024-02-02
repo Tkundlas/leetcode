55aac8c2144788648fd6166104ad1f402797008b
Tanuj <101932530+Tkundlas@users.noreply.github.com>
2024-02-02 23:39:06 +0530

    Time: 300 ms (5.03%), Space: 95.8 MB (20.06%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,9 +5,10 @@ public:
     int n = arr.size();
     int minPrice = INT_MAX;
 
-    for (int i = 0; i < arr.size(); i++) {
+    for (int i = 0; i < n; i++) {
         minPrice = min(minPrice, arr[i]);
         maxPro = max(maxPro, arr[i] - minPrice);
+        cout<< maxPro <<" ";
     }
     
     return maxPro;
