44c55b7b1076a14190195524e79407ccc1858b42
Tanuj <101932530+Tkundlas@users.noreply.github.com>
2024-03-12 11:48:10 +0530

    Time: 58 ms (35.87%), Space: 25.7 MB (28.52%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isPalindrome(string s, int i, int j)
+    {  
+        while(i<=j)
+        {
+            if(s[i] != s[j]) return false;
+            
+            else{
+                i++;
+                j--;
+            }
+        }
+        
+        return true;
+    }
+    
+public:
+    bool validPalindrome(string s) {
+        
+        int i = 0, j = s.length() - 1;
+        
+        while(i<=j)
+        {
+            if( s[i] != s[j] ){return isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1);} 
+            
+            else{
+                i++;
+                j--;
+            }
+        }
+        
+        return true;
+    }
+};
